9a2132a776744cb1792e50e6e6e13c1abc7f5bf9
kishkin.a <kishkin.a@gmail.com>
2023-10-20 13:08:44 +0300

    Перший коміт: додано Vue.js додаток

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <p>Спеціальність: 121 Інженерія програмного забезпечення</p>
+    <p>Курс: 2</p>
+    <p>Група: 8.1212</p>
+    <p>ПІБ: Кішкін О.І.</p>
+    </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p>Спеціальність: 121 Інженерія програмного забезпечення</p>
+    <p>Курс: 2</p>
+    <p>Група: 8.1212</p>
+    <p>ПІБ: Кішкін О.І.</p>
+  </div>
+</template>
